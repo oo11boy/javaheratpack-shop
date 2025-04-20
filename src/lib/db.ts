81c7b25e0cb8a6d5b94ad506c3a9bac6282e1a85
oo11boy <editkhone@gmail.com>
2025-04-21 00:53:16 +0430

    update

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,12 @@
 import mysql from "mysql2/promise";
 
 const dbConfig = {
-  host: "siah-kaman.liara.cloud", 
-  user: "root",
-  password: "Xi2YKsy6TZf2VAZF1qp30nsz",
-  database: "epic_mccarthy",
-  port: 32173,
+  host: "45.139.10.148",
+  user: "javaherat",
+  password: "Ra1378!AbCd#1379",
+  database: "javaherat",
+  port: 3306,
 };
-
 
 export async function getConnection() {
   return await mysql.createConnection(dbConfig);
