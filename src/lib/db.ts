d217a5b542995ba54bf7eee8805faad8a525914f
oo11boy <editkhone@gmail.com>
2026-06-21 13:50:07 +0330

    fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,30 +1,14 @@
 import mysql from "mysql2/promise";
 
 const dbConfig = {
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "javaheratnew1245",
-  port: 3306,
+  host: process.env.DBHOST || "",
+  user: process.env.DBUSER || "",
+  password: process.env.DBPASSWORD || "",
+  database: process.env.DBNAME || "",
+  port: parseInt(process.env.DBPORT || "3306"),
 };
 
 export async function getConnection() {
   return await mysql.createConnection(dbConfig);
 }
 
-
-
-// import mysql from "mysql2/promise";
-
-// const dbConfig = {
-//   host: "siah-kaman.liara.cloud", 
-//   user: "root",
-//   password: "Xi2YKsy6TZf2VAZF1qp30nsz",
-//   database: "epic_mccarthy",
-//   port: 32173,
-// };
-
-
-// export async function getConnection() {
-//   return await mysql.createConnection(dbConfig);
-// }
